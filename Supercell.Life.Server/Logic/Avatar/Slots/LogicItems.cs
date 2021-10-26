10b45073599b366bd00f8f01a7a2dadf8d389cbe
kylecourounis <kyledoescode@gmail.com>
2021-10-26 08:02:06 -0400

    Made some changes to LogicItems

--- Supercell.Life.Server/Logic/Avatar/Slots/LogicItems.cs
+++ Supercell.Life.Server/Logic/Avatar/Slots/LogicItems.cs
@@ -4,25 +4,28 @@
 
     using Supercell.Life.Titan.Logic.Json;
 
+    using Supercell.Life.Server.Files;
+    using Supercell.Life.Server.Files.CsvLogic;
     using Supercell.Life.Server.Logic.Avatar.Items;
+    using Supercell.Life.Server.Logic.Enums;
 
     internal class LogicItems
     {
         internal readonly LogicClientAvatar Avatar;
 
-        #region Item IDs
-        internal const int EnergyRecycler  = 37000000;
-        internal const int PlunderThunder  = 37000001;
-        internal const int RobbersHood     = 37000002;
-        internal const int LuckyFix        = 37000003;
-        internal const int CaptainsBounce  = 37000004;
-        internal const int GalaxyDefender  = 37000005;
-        internal const int MegaFlex        = 37000006;
-        internal const int PowerProtector  = 37000007;
-        internal const int ComboCruncher   = 37000008;
-        internal const int AdventurersSnap = 37000009;
-        internal const int SmackAttack     = 37000010;
-        internal const int BubbleArmor     = 37000011;
+        #region ItemsData
+        internal static readonly LogicItemsData EnergyRecycler  = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("GetEnergy");
+        internal static readonly LogicItemsData PlunderThunder  = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("GetXP");
+        internal static readonly LogicItemsData RobbersHood     = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("GetGold");
+        internal static readonly LogicItemsData LuckyFix        = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("BarrelOdds");
+        internal static readonly LogicItemsData CaptainsBounce  = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("PlusCombo");
+        internal static readonly LogicItemsData GalaxyDefender  = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("HPPlus");
+        internal static readonly LogicItemsData MegaFlex        = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("SpecialAttackPlus");
+        internal static readonly LogicItemsData PowerProtector  = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("GetHpEnemyCombo");
+        internal static readonly LogicItemsData ComboCruncher   = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("StartCombo");
+        internal static readonly LogicItemsData AdventurersSnap = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("DoubleDmg");
+        internal static readonly LogicItemsData SmackAttack     = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("DmgPlus");
+        internal static readonly LogicItemsData BubbleArmor     = (LogicItemsData)CSV.Tables.Get(Gamefile.Items).GetDataByName("Shield");
         #endregion
 
         /// <summary>
@@ -43,7 +46,7 @@
                 int percentage = 5;
                 int increment = 2;
 
-                for (int i = 1; i <= this.Avatar.ItemLevels.GetCount(LogicItems.PlunderThunder); i++)
+                for (int i = 1; i <= this.Avatar.ItemLevels.GetCount(LogicItems.PlunderThunder.GlobalID); i++)
                 {
                     percentage += increment;
                     increment++;
@@ -56,9 +59,9 @@
         /// <summary>
         /// Determines whether the item with the specified identifier is attached to any of the heroes in the player's loadout. 
         /// </summary>
-        internal bool IsAttached(int itemId)
+        internal bool IsAttached(LogicItemsData itemData)
         {
-            return this.Avatar.ItemAttachedTo.Values.Any(item => item.Id.Equals(this.Avatar.ItemLevels.Get(itemId).Id) && this.Avatar.Team.Any(hero => hero.ToObject<int>().Equals(item.Count)));
+            return this.Avatar.ItemAttachedTo.Values.Any(item => item.Id.Equals(this.Avatar.ItemLevels.Get(itemData.GlobalID).Id) && this.Avatar.Team.Any(hero => hero.ToObject<int>().Equals(item.Count)));
         }
 
         /// <summary>
